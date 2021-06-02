eed0ba01b0cb818c4d7782e3885fba543fd8eaac
Igal Sokolovsky <igal.s.1994@gmail.com>
2021-06-02 19:09:14 +0300

    Update Application.jsx

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from '../Login/Login'
-import SignUp from '../SignUp/SignUp'
+import Login from '../Login/Login';
+import SignUp from '../SignUp/SignUp';
 import Home from '../Home/Home';
+import Upload from '../Upload/Upload';
 function Application() {
 	return (
 		<div>
 			<Router>
 				<Switch>
-					<Route path="/login" component={Login}/>
-					<Route path="/signup" component={SignUp}/>
+					<Route path="/login" component={Login} />
+					<Route path="/upload" component={Upload} />
+					<Route path="/signup" component={SignUp} />
 					<Route path="/" component={Home} />
 				</Switch>
 			</Router>
