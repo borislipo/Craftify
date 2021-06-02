7c953bb3e187d09b50eecd895829cc74bd6d7534
Saher Dallasheh <77542961+SaherDallasheh@users.noreply.github.com>
2021-06-02 16:23:43 +0300

    intial layout with onclick function on upload images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Application from './components/Application/Application';
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,51 @@
-function Home() {
-	return <div />;
+import React from "react";
+import { Row, Col, Container, Button, Image } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import "./Home.css";
+
+
+function Home(props) {
+	const history = useHistory();
+	const { userName } = props;
+
+	const handleClick = () => {
+		history.push('/');
+	}
+
+
+	return (
+		<>
+		
+		<Row className="HeaderContainer" >
+		<Col>
+		<Image placeholder="Logo" />
+		</Col>
+		<Col>
+		<h1>Some User</h1>
+		</Col>
+		</Row>
+
+		<Container fluid className="HomeContainer">
+		<Row className="mb-5 mt-5 d-flex HomeButton">
+		<Col><Button  variant="primary" onClick={handleClick}>Upload Images</Button></Col>
+		<Col><Button  variant="primary">Craftify!</Button></Col>
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		</Row>
+		
+		<Row className="mb-5 mt-5 d-flex HomeButton">
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		<Col></Col>
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		</Row>
+		
+		<Row className="mb-5 mt-5 d-flex HomeButton">
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		<Col><Button variant="primary">Other Feature</Button></Col>
+		</Row>
+		</Container>
+		</>
+	);
 }
+
 export default Home;
