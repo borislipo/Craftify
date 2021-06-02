dbb731b988e0746dbfc9566ab31ad6134257b8ed
Jade Cohen <jadecohen97@gmail.com>
2021-06-02 19:04:07 +0300

    update

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from '../Login/Login'
-import SignUp from '../SignUp/SignUp'
-import Home from '../Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "../Login/Login";
+import SignUp from "../SignUp/SignUp";
+import Home from "../Home/Home";
+import Upload from "../Upload/Upload";
 function Application() {
-	return (
-		<div>
-			<Router>
-				<Switch>
-					<Route path="/login" component={Login}/>
-					<Route path="/signup" component={SignUp}/>
-					<Route path="/" component={Home} />
-				</Switch>
-			</Router>
-		</div>
-	);
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/upload" component={Upload} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 export default Application;
